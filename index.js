83dcb448e8d9af9ac369c93dd3b632c8c7ebb11a
Scott Enslin <enslin21@yahoo.com>
2018-07-09 18:08:17 -0400

    add server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+let express = require("express");
+const app = express();
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
