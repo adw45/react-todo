c769835d50edce10ecdeb1134b69c41b3866b58f
Scott Enslin <Enslin21@yahoo.com>
2018-07-10 17:26:31 -0400

    Express servers static pages (#2)
    * add server
    
    * add static html to server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+let express = require("express");
+const app = express();
+
+
+app.use(express.static('./app'));
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
